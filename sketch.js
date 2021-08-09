476e92dfccdfebb53b4b53dd7b36057095be0ed4
nishant31jan <87146449+nishant31jan@users.noreply.github.com>
2021-08-09 12:18:24 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,155 +1,146 @@
+var path,boy,cash,diamonds,jwellery,sword;
+var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
+var treasureCollection = 0;
+var cashG,diamondsG,jwelleryG,swordGroup;
+
 //Game States
 var PLAY=1;
 var END=0;
 var gameState=1;
 
-var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
-var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage, knifeSound, gameSound;
-
 function preload(){
-  
-  knifeImage = loadImage("knife.png");
-  monsterImage = loadAnimation("alien1.png","alien2.png")
-  fruit1 = loadImage("fruit1.png");
-  fruit2 = loadImage("fruit2.png");
-  fruit3 = loadImage("fruit3.png");
-  fruit4 = loadImage("fruit4.png");
-  gameOverImage = loadImage("gameover.png");
- 
-  //load sound here
-  knifeSound=loadSound("knifeSwoosh.mp3");
-  gameSound=loadSound("gameover.mp3");
-
+  pathImg = loadImage("Road.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jwelleryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  endImg =loadAnimation("gameOver.png");
 }
 
+function setup(){
+  
+  createCanvas(windowWidth,windowHeight);
+// Moving background
+path=createSprite(width/2,200);
+path.addImage(pathImg);
+path.velocityY = 4;
 
 
-function setup() {
-  createCanvas(600, 600);
+//creating boy running
+boy = createSprite(width/2,height-20,20,20);
+boy.addAnimation("SahilRunning",boyImg);
+boy.scale=0.08;
   
-  //creating sword
-   knife=createSprite(40,200,20,20);
-   knife.addImage(knifeImage);
-   knife.scale=0.7
   
-  //set collider for sword
-  knife.setCollider("rectangle",0,0,40,40);
+cashG=new Group();
+diamondsG=new Group();
+jwelleryG=new Group();
+swordGroup=new Group();
 
-  // Score variables and Groups
-  score=0;
-  fruitGroup=createGroup();
-  monsterGroup=createGroup();
-  
 }
 
 function draw() {
-  background("lightblue");
-  
+
   if(gameState===PLAY){
-    
-    //Call fruits and Monster function
-    fruits();
-    Monster();
-    
-    // Move sword with mouse
-    knife.y=World.mouseY;
-    knife.x=World.mouseX;
-    
+  background(0);
+  boy.x = World.mouseX;
   
-    // Increase score if sword touching fruit
-    if(fruitGroup.isTouching(knife)){
-      fruitGroup.destroyEach();
-      knifeSound.play();
-      score=score+2
-      fruitGroup.velocityX=-(8+(score/4));
+  edges= createEdgeSprites();
+  boy.collide(edges);
+  
+  //code to reset the background
+  if(path.y > height){
+    path.y = height/2;
+  }
+  
+    createCash();
+    createDiamonds();
+    createJwellery();
+    createSword();
+
+    if (cashG.isTouching(boy)) {
+      cashG.destroyEach();
+      treasureCollection=treasureCollection+50;
     }
-    else
-    {
-     
-     
-      // Go to end state if sword touching enemy
-      if(monsterGroup.isTouching(knife)){
+    else if (diamondsG.isTouching(boy)) {
+      diamondsG.destroyEach();
+      treasureCollection=treasureCollection+100;
+      
+    }else if(jwelleryG.isTouching(boy)) {
+      jwelleryG.destroyEach();
+      treasureCollection= treasureCollection + 150;
+      
+    }else{
+      if(swordGroup.isTouching(boy)) {
         gameState=END;
         
-        //add gameover sound here
-        gameSound.play();
-        fruitGroup.destroyEach();
-        monsterGroup.destroyEach();
-        fruitGroup.setVelocityXEach(0);
-        monsterGroup.setVelocityXEach(0);
+        boy.addAnimation("SahilRunning",endImg);
+        boy.x=width/2;
+        boy.y=height/2;
+        boy.scale=0.6;
         
-        // Change the animation of sword to gameover and reset its position
-        knife.addImage(gameOverImage);
-        knife.scale=2;
-        knife.x=300;
-        knife.y=300;
-        monster.velocityX=-(8+(score/10));
-      }
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jwelleryG.destroyEach();
+        swordGroup.destroyEach();
+        
+        cashG.setVelocityYEach(0);
+        diamondsG.setVelocityYEach(0);
+        jwelleryG.setVelocityYEach(0);
+        swordGroup.setVelocityYEach(0);
+     
     }
-    
   }
   
   drawSprites();
-  //Display score
-  textSize(25);
-  text("Score : "+ score,250,50);
+  textSize(20);
+  fill(255);
+  text("Treasure: "+ treasureCollection,width/2-150,height-700);
+  }
+
 }
 
-
-function Monster(){
-  if(World.frameCount%200===0){
-    monster=createSprite(400,200,20,20);
-    monster.addAnimation("moving", monsterImage);
-    monster.y=Math.round(random(100,550));
-    //update below give line of code for increase monsterGroup speed by 10
-    monster.velocityX = -8;
-    monster.setLifetime=50;
-    
-    monsterGroup.add(monster);
+function createCash() {
+  if (World.frameCount % 200 == 0) {
+  var cash = createSprite(Math.round(random(50,width-50),40, 10, 10));
+  cash.addImage(cashImg);
+  cash.scale=0.12;
+  cash.velocityY = 3;
+  cash.lifetime = height;
+  cashG.add(cash);
   }
 }
 
-function fruits(){
-  if(World.frameCount%80===0){
-    position = Math.round(random(1,2));
-    fruit=createSprite(400,200,20,20);
-    
-     //using random variable change the position of fruit, to make it more challenging
-    
-    if(position==1)
-    {
-    fruit.x=600;
-    //update below give line of code for increase fruitGroup speed by 4
-    fruit.velocityX=-7
-    }
-    else
-    {
-      if(position==2){
-      fruit.x=0;
-      
-     //update below give line of code for increase fruitGroup speed by 4
-      fruit.velocityX= 7;
-      }
-    }
-    
-    fruit.scale=0.2;
-     //fruit.debug=true;
-     r=Math.round(random(1,4));
-    if (r == 1) {
-      fruit.addImage(fruit1);
-    } else if (r == 2) {
-      fruit.addImage(fruit2);
-    } else if (r == 3) {
-      fruit.addImage(fruit3);
-    } else {
-      fruit.addImage(fruit4);
-    }
-    
-    fruit.y=Math.round(random(50,550));
-   
-    
-    fruit.setLifetime=100;
-    
-    fruitGroup.add(fruit);
+function createDiamonds() {
+  if (World.frameCount % 320 == 0) {
+  var diamonds = createSprite(Math.round(random(50,width-50),40, 10, 10));
+  diamonds.addImage(diamondsImg);
+  diamonds.scale=0.03;
+  diamonds.velocityY = 3;
+  diamonds.lifetime = width;
+  diamondsG.add(diamonds);
+}
+}
+
+function createJwellery() {
+  if (World.frameCount % 410 == 0) {
+  var jwellery = createSprite(Math.round(random(50,width-50),40, 10, 10));
+  jwellery.addImage(jwelleryImg);
+  jwellery.scale=0.13;
+  jwellery.velocityY = 3;
+  jwellery.lifetime =width;
+  jwelleryG.add(jwellery);
+  }
+}
+
+function createSword(){
+  if (World.frameCount % 530 == 0) {
+  var sword = createSprite(Math.round(random(50,width-50),40, 10, 10));
+  sword.addImage(swordImg);
+  sword.scale=0.1;
+  sword.velocityY = 3;
+  sword.lifetime = width;
+  swordGroup.add(sword);
   }
 }
